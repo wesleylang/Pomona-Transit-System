eb281b5548327e99a1b0ab29c47de263e42201f5
WJBen <ningli@cpp.edu>
2016-12-07 02:15:17 -0800

    Update

--- src/PomonaTransit.java
+++ src/PomonaTransit.java
@@ -27,8 +27,9 @@ public class PomonaTransit {
 		menu();
 	}
 	
+	//display the method and get user command
 	public static void menu() {
-		System.out.println("What would you like to do?");
+		System.out.println("Please choose from the following:");
 		System.out.println("1 - Display schedule for all trips.");
 		System.out.println("2 - Edit the schedule");
 		System.out.println("3 - Display the stops of a given trip");
@@ -37,94 +38,92 @@ public class PomonaTransit {
 		System.out.println("6 - Add a bus");
 		System.out.println("7 - Delete a bus");
 		System.out.println("8 - Record data of a given trip");
-		System.out.println("0 - Exit");
-
+		System.out.println("0 - Exit program");
+		System.out.println();
 		
 		Scanner in = new Scanner(System.in);
 		int input = in.nextInt();
-		System.out.println();
 		
-		switch(input) {
-		case 0: System.exit(0);
-			break;
-		case 1: 
+		//call functions accordingly to user's input then keep the ask again unless
+		//user enters 0 to exit the program
+		if(input == 0) {
+			System.exit(0);
+		}
+		else if(input == 1) { 
 			displaySchedule();
 			System.out.println();
 			menu();
-			break;
-		case 2: 
+		}
+		else if(input == 2){ 
 			editSchedule();
 			System.out.println();
 			menu();
-			break;
-		case 3: 
+		}
+		else if(input == 3){ 
 			displayStops();
 			System.out.println();
 			menu();
-			break;
-		case 4: 
+		}
+		else if(input == 4){ 
 			displayWeeklySchedule();
 			System.out.println();
 			menu();
-			break;
-		case 5: 
+		}
+		else if(input == 5){ 
 			addDriver();
 			System.out.println();
 			menu();
-			break;
-		case 6: 
+		}
+		else if(input == 6){ 
 			addBus();
 			System.out.println();
 			menu();
-			break;
-		case 7: 
+		}
+		else if(input == 7){ 
 			deleteBus();
 			System.out.println();
 			menu();
-			break;
-		case 8: 
+		}
+		else if(input == 8){ 
 			recordData();
 			System.out.println();
 			menu();
-			break;
-		default: System.out.println("Invalid input. Please try again.");
+		}
+		else{
+			System.out.println("Invalid input. Please try again.");
 			menu();
-			break;
 		}
 		in.close();
 	}
 
 	private static void displaySchedule() {
-		//display the schedule of all trips for a given start location, destination
-		//and date. In addition schedule includes schedules start, scheduled arrival,
-		//driver id and bus id.
-		
+		//ask the user for start location, destination, and date, then display
+		//the corresponding schedule with all information of that particlar trip.
 		String startLocationName, destination, date;
 		Scanner in = new Scanner(System.in);
 		System.out.print("Start Location Name: ");
 		startLocationName = in.next();
-		
 		System.out.print("Destination Name: ");
 		destination = in.next();
-		
 		System.out.print("Date (YYYY-MM-DD): ");
 		date = in.next();
 		
 		String query;
-		query = "SELECT t.TripNumber, t.StartLocationName, t.DestinationName, o.Date"
-				+ ", o.ScheduledStartTime, o.ScheduledArrivalTime, o.DriverName, o.BusId "
-				+ "FROM Trip t, TripOffering o "
-				+ "WHERE t.TripNumber = o.TripNumber AND "
+		query = "SELECT t.TripNumber, t.StartLocationName, t.DestinationName, to.Date"
+				+ ", to.ScheduledStartTime, to.ScheduledArrivalTime, to.DriverName, to.BusId "
+				+ "FROM Trip t, TripOffering to "
+				+ "WHERE t.TripNumber = to.TripNumber AND "
 				+ "t.StartLocationName LIKE '" + startLocationName + "' AND "
 				+ "t.DestinationName" + " LIKE '" + destination + "' "+ " AND "
-				+ "o.Date LIKE '" + date + "'";
+				+ "to.Date LIKE '" + date + "'";
 		
 		sqlDisplayer(query);
 		in.close();
 	}
 	
 	private static void editSchedule() {
-		System.out.println("Schedule Editing Options: ");
+		//display option to which information that the user would like to edit
+		System.out.println("Editing Options: ");
 		System.out.println("1 - Delete a trip offering specified by Trip#, Date, and ScheduledStartTime.");
 		System.out.println("2 - Add a set of trip offerings.");
 		System.out.println("3 - Change the driver for a given trip.");
@@ -133,23 +132,23 @@ public class PomonaTransit {
 		Scanner in = new Scanner(System.in);
 		int input = in.nextInt();
 		
-		switch(input) {
-		case 1: deleteTripOffering();
-			break;
-		case 2: addTripOfferings();
-			break;
-		case 3: changeDriver();
-			break;
-		case 4: changeBus();
-			break;
-		default: System.out.println("Invalid input. Try again.");
+		if(input == 1)
+			deleteTripOffering();
+		else if(input == 2)
+			addTripOfferings();
+		else if(input == 3)
+			changeDriver();
+		else if(input == 4)
+			changeBus();
+		else {
+			System.out.println("Invalid input. Try again.");
 			editSchedule();
-			break;
 		}
 		in.close();
 	}
 	
 	private static void deleteTripOffering() {
+		//this function deletes a tripoffering correspond to user's choice
 		String sqlVar;
 		int tripNumber;
 		String date;
@@ -158,10 +157,8 @@ public class PomonaTransit {
 	
 		System.out.print("TripNumber: ");	
 		tripNumber = in.nextInt();
-		
 		System.out.print("Date(Please follow 'YYYY-MM-DD' format): ");	
 		date = in.next();
-		
 		System.out.print("ScheduledStartTime(Please follow 'HH:MM' 24H format): ");	
 		scheduledStartTime = in.next() + ":00";
 	
@@ -172,11 +169,12 @@ public class PomonaTransit {
 				+"';";
 		sqlHandler(sqlVar);
 		
-		System.out.println("Trip deleted!");
+		System.out.println("Trip Deleted Successfully!");
 		in.close();
 	}
 
 	private static void addTripOfferings() {
+		//this function adds a tripoffering according to user's choice
 		String sqlVar;
 		int tripNumber;
 		int busID;
@@ -187,19 +185,14 @@ public class PomonaTransit {
 	
 		System.out.print("TripNumber: ");	
 		tripNumber = in.nextInt();
-		
 		System.out.print("Date(Please follow 'YYYY-MM-DD' format): ");	
 		date = in.next();
-		
 		System.out.print("ScheduledStartTime(Please follow 'HH:MM' 24H format): ");	
 		scheduledStartTime = in.next() + ":00";
-		
 		System.out.print("ScheduledArrivalTime(Please follow 'HH:MM' 24H format): ");	
 		scheduledArrivalTime = in.next() + ":00";
-		
 		System.out.print("DriverName: ");	
 		driverName = in.next();
-		
 		System.out.print("BusID: ");	
 		busID = in.nextInt();
 	
@@ -213,13 +206,14 @@ public class PomonaTransit {
 				+");";
 		sqlHandler(sqlVar);
 		
-		System.out.println("Trip Added! Would you like to add another? ('Y' or 'N')");
+		System.out.println("Trip Added Successfully! Add another? ('Y' or 'N')");
 		String input = in.next();
 		if(input.toUpperCase().equals("Y"))addTripOfferings();
 		in.close();
 	}
 
 	private static void changeDriver() {
+		//this function change the driver with user's input
 		String sqlVar;
 		int tripNumber;
 		String date;
@@ -229,13 +223,10 @@ public class PomonaTransit {
 	
 		System.out.print("TripNumber: ");	
 		tripNumber = in.nextInt();
-		
 		System.out.print("Date(Please follow 'YYYY-MM-DD' format): ");	
 		date = in.next();
-		
 		System.out.print("ScheduledStartTime(Please follow 'HH:MM' 24H format): ");	
 		scheduledStartTime = in.next() + ":00";
-		
 		System.out.print("New Bus Driver: ");
 		driverName = in.next();
 	
@@ -248,11 +239,12 @@ public class PomonaTransit {
 				+"';";
 		sqlHandler(sqlVar);
 		
-		System.out.println("Driver Changed!");
+		System.out.println("Driver Changed Successfully!");
 		in.close();
 	}
 
 	private static void changeBus() {
+		//this function changes the bus with user's input
 		String sqlVar;
 		int tripNumber;
 		String date;
@@ -262,13 +254,10 @@ public class PomonaTransit {
 	
 		System.out.print("TripNumber: ");	
 		tripNumber = in.nextInt();
-		
 		System.out.print("Date(Please follow 'YYYY-MM-DD' format): ");	
 		date = in.next();
-		
 		System.out.print("ScheduledStartTime(Please follow 'HH:MM' 24H format): ");	
 		scheduledStartTime = in.next() + ":00";
-		
 		System.out.print("New Bus: ");
 		busID = in.nextInt();
 	
@@ -281,38 +270,34 @@ public class PomonaTransit {
 				+"';";
 		sqlHandler(sqlVar);
 		
-		System.out.println("Bus Changed!");
+		System.out.println("Bus Changed Successfully!");
 		in.close();
 	}
 
 	private static void displayStops() {
+		//this function displays the stops of a trip with user's input
 		String query;
 		int tripNumber;
 		Scanner in = new Scanner(System.in);
 	
 		System.out.print("TripNumber: ");	
 		tripNumber = in.nextInt();
-		
 		query = "SELECT * FROM TripStopInfo WHERE TripNumber=" + tripNumber + ";";
-		sqlDisplayer(query);
 		
+		sqlDisplayer(query);
 		in.close();
 	}
 	
 	private static void displayWeeklySchedule() {
-		// display the weekly schedule of a given driver and date
-		
-		//sort trip offering by given name, then date
+		//this function display the weekly schedule with user's input of name and date
+		//then sorts trip offering by given name, then date
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter in a driver's name: ");
 		String driverName = in.next();
-				
 		System.out.print("Given month(two digits): ");
 		String month = in.next();
-		
 		System.out.print("Given day(two digits): ");
 		int day = in.nextInt();
-				
 		System.out.print("Given year (two digits): ");
 		String year = in.next();
 				
@@ -334,6 +319,7 @@ public class PomonaTransit {
 	}
 	
 	private static void addDriver() {
+		//this function add a driver per user's request with name and phone number
 		String driverName;
 		String sqlVar;
 		String driverTelephoneNumber;
@@ -341,19 +327,17 @@ public class PomonaTransit {
 
 		System.out.print("DriverName: ");	
 		driverName = in.next();
-		
 		System.out.print("DriverTelephoneNumber (Please follow XXX-XXXX format: ");
 		driverTelephoneNumber = in.next();
-		
 		sqlVar = "INSERT INTO DRIVER VALUES('" + driverName + "', '" + driverTelephoneNumber + "');";
 		sqlHandler(sqlVar);
 		
-		System.out.println("Driver added!");
+		System.out.println("Driver Added Successfully!");
 		in.close();
 	}
 	
 	private static void addBus() {
-		// INSERT INTO 'pomona_transit'.'bus'('BusID', 'Model', 'Year');
+		//this function add a bus according to user's input of new bus ID, model and year
 		String model = "NULL";
 		String sqlVar;
 		int busID = 0;
@@ -362,38 +346,36 @@ public class PomonaTransit {
 
 		System.out.print("BusID: ");	
 		busID = in.nextInt();
-		
 		System.out.print("Model: ");	
 		model = in.next();
-		
 		System.out.print("Year: ");
 		year = in.nextInt();
 		
 		sqlVar = "INSERT INTO BUS VALUES('" + busID + "', '" + model + "', '" + year + "');";
 		sqlHandler(sqlVar);
 		
-		System.out.println("Bus added!");
+		System.out.println("Bus Added Successfully!");
 		in.close();
 	}
 	
 	
 	private static void deleteBus() {
+		//this method prompts user for bus ID to delete that record
 		String sqlVar;
 		int busID;
 		Scanner in = new Scanner(System.in);
 	
 		System.out.print("BusID: ");	
 		busID = in.nextInt();
-	
 		sqlVar = "DELETE FROM BUS WHERE BusID='" + busID + "';";
 		sqlHandler(sqlVar);
 		
-		System.out.println("Bus deleted!");
+		System.out.println("Bus Deleted Successfully!");
 		in.close();
 	}
 
 	private static void recordData() {
-		// insert the actual data of a given trip offering specified by its key. 
+		//this function prompts user to insert the actual data of a given trip offering specified by its key. 
 		//the actual data include the attributes of the table actual trip stop info
 		
 		int tripNumber;
@@ -403,10 +385,8 @@ public class PomonaTransit {
 		
 		System.out.print("TripNumber: ");
 		tripNumber = in.nextInt();
-		
 		System.out.print("Date (YYYY-MM-DD): ");
 		date = in.next();
-		
 		System.out.print("ScheduledStartTime (HH:MM:SS): ");
 		scheduledStartTime = in.next();
 		
@@ -440,7 +420,7 @@ public class PomonaTransit {
 				stopNumber = rs.getString(2);
 			}
 			
-			//Clean up
+			//closes resultSet and connection
 			rs.close();
 			conn.close();			
 		} catch(Exception e){
@@ -517,7 +497,7 @@ public class PomonaTransit {
 				System.out.println();
 			}
 			
-			//Clean up
+			//closes Resultset and connection
 			rs.close();
 			conn.close();			
 		} catch(Exception e){
